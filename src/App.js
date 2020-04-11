2d3cf4c76b50e2a83b296380f66098e4e53fe5ea
Joe Santhosh <joe.santhosh88@yahoo.com>
2020-04-10 21:55:41 -0400

    added redux implementation to our App and added currentUser state from user reducer to Header and added setCurrentUser from user action to action to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import Navbar from './components/navbar/navbar.component';
 import RegistrationAndLogin from './pages/registration-and-login/registration-and-login.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends Component {
   constructor() {
@@ -20,22 +23,22 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
         });
       }
 
       else {
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -46,15 +49,19 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/registerandsignin' component={RegistrationAndLogin} />
-        </Switch>
-      </div>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/registerandsignin' component={RegistrationAndLogin} />
+          </Switch>
+        </div>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './navbar.styles.scss';
 
 import { Link } from 'react-router-dom';
 
-const Navbar = () => (
+import { auth } from '../../firebase/firebase.utils';
+
+const Navbar = ({ currentUser }) => (
     <nav className="navbar">
         <Link className="home-link" to="/">
             Home
         </Link>
         <div className="nav-options">
             <Link className="option" to="/contact">CONTACT US</Link>
-            <Link className="option" to="/registerandsignin">LOG IN</Link>
+            {
+                !currentUser ?
+                <Link className="option" to="/registerandsignin">LOG IN</Link> :
+                (<div className="option" onClick={() => auth.signOut()}>
+                    SIGN OUT
+                </div>)   
+            }
         </div>
     </nav>
 );
 
-export default Navbar;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Navbar);
