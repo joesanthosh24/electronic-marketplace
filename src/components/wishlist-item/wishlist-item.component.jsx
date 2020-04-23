e619d8f0eb55119a5a20084166f5da537f27b9a1
Joe Santhosh <joe.santhosh88@yahoo.com>
2020-04-23 10:39:03 -0400

    Added wishlist-item component

---
+++ src/components/wishlist-item/wishlist-item.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './wishlist-item.styles.scss';
+
+const WishlistItem = () => (
+    <div className="wishlist-item">
+        <span>Item</span>
+    </div>
+);
+
+export default WishlistItem;
