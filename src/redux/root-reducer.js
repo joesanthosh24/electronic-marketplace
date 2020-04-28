02eb0e87580109cf4f15b6ba0c8ae377751ff3ae
Joe Santhosh <joe.santhosh88@yahoo.com>
2020-04-28 19:50:14 -0400

    Made redux state persistent using redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,13 +1,23 @@
 import { combineReducers } from "redux";
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from "./user/user.reducer";
 import sectionReducer from "./sections/sections.reducer";
 import shopCollectionsReducer from "./shop-collections/shop-collections.reducer";
 import cartWishlistReducer from "./cart-wishlist/cart-wishlist.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['listItems']
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   directory: sectionReducer,
   shop: shopCollectionsReducer,
   listItems: cartWishlistReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
