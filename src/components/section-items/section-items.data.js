7ed1e18a7c2d5d57e3d17904522b06d2171dde1e
Joe Santhosh <joe.santhosh88@yahoo.com>
2023-07-08 12:28:32 -0400

    Changed headphones image

--- src/components/section-items/section-items.data.js
+++ src/components/section-items/section-items.data.js
@@ -1,28 +1,32 @@
 const items = [
-    {
-        title: 'Laptops',
-        imageUrl: "https://512pixels.net/wp-content/uploads/2019/02/18-air-open.jpg",
-        id: 1,
-        linkUrl: '/laptops'
-    },
-    {
-        title: 'Games',
-        imageUrl: "https://www.reviewgeek.com/thumbcache/0/0/7ff43d8ef852cbc3f10efa11652965d7/p/uploads/2018/06/40e7257e.jpg",
-        id: 2,
-        linkUrl: '/video games'
-    },
-    {
-        title: 'Headphones',
-        imageUrl: 'https://cnet2.cbsistatic.com/img/fib6tyPkwi4yxbrSHiuhw27Nwo0=/1092x0/2019/11/19/ce454fb4-2ba7-4853-b5cd-a115740ef15e/anker-soundcore-life-q10-promo.png',
-        id: 3,
-        linkUrl: '/head phones'
-    },
-    {
-        title: 'Phones',
-        imageUrl: 'https://sm.mashable.com/t/mashable_sea/image/t/the-best-c/the-best-cyber-monday-smartphone-deals-are-right-here_ejs9.960.jpg',
-        id: 4,
-        linkUrl: '/phones'
-    }
+  {
+    title: "Laptops",
+    imageUrl:
+      "https://512pixels.net/wp-content/uploads/2019/02/18-air-open.jpg",
+    id: 1,
+    linkUrl: "/laptops",
+  },
+  {
+    title: "Games",
+    imageUrl:
+      "https://www.reviewgeek.com/thumbcache/0/0/7ff43d8ef852cbc3f10efa11652965d7/p/uploads/2018/06/40e7257e.jpg",
+    id: 2,
+    linkUrl: "/video games",
+  },
+  {
+    title: "Headphones",
+    imageUrl:
+      "https://www.reviewgeek.com/p/uploads/2023/02/55911c48.png?width=1200",
+    id: 3,
+    linkUrl: "/head phones",
+  },
+  {
+    title: "Phones",
+    imageUrl:
+      "https://sm.mashable.com/t/mashable_sea/image/t/the-best-c/the-best-cyber-monday-smartphone-deals-are-right-here_ejs9.960.jpg",
+    id: 4,
+    linkUrl: "/phones",
+  },
 ];
 
 export default items;
